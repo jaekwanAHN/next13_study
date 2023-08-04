bdcc20790c2d81103ad2dee597b695d54936bf9f
JaeKwan Ahn <52008@naver.com>
2023-08-04 19:25:26 +0900

    Test: getStaticProps

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,5 +1,24 @@
-const Temp = () => {
-  return <div>난 테스트 페이지이다</div>;
+interface TestPostProps {
+  TestPost: string;
+}
+
+const TestPost = ({ TestPost }: TestPostProps) => {
+  return (
+    <div>
+      TEST
+      <div>{TestPost}</div>
+    </div>
+  );
 };
 
-export default Temp;
+export async function getStaticProps() {
+  // By returning { props: { posts } }, the Blog component
+  // will receive `posts` as a prop at build time
+  return {
+    props: {
+      TestPost: "나는 테스트 메시지이다",
+    },
+  };
+}
+
+export default TestPost;
